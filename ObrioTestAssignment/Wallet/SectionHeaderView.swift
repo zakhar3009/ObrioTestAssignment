4fc56e7407450291c0e677067365148c98e17985
zakharlit <zakhlit@gmail.com>
2025-04-03 15:19:50 +0300

    Create transaction group header view

---
+++ ObrioTestAssignment/Wallet/SectionHeaderView.swift
@@ -0,0 +1,40 @@
+//
+//  SectionHeaderView.swift
+//  ObrioTestAssignment
+//
+//  Created by Zakhar Litvinchuk on 03.04.2025.
+//
+
+import UIKit
+
+class SectionHeaderView: UICollectionReusableView {
+    let label = UILabel()
+    static let reuseIdentifier = "title-supplementary-reuse-identifier"
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    func configure(text: String) {
+        label.text = text
+    }
+    
+    func setupUI() {
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        addSubview(label)
+        let inset = CGFloat(10)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: inset),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -inset),
+            label.topAnchor.constraint(equalTo: topAnchor, constant: inset),
+            label.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -inset)
+        ])
+    }
+}
